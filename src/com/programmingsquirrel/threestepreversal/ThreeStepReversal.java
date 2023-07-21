1b0a9b1a5385b3f66902d6fb64148b7c24ce9204
cesarpasillas <cesar.epasillas@gmail.com>
2023-07-21 17:48:57 -0500

    Initial commit for the Algorithm_techniques project

---
+++ src/com/programmingsquirrel/threestepreversal/ThreeStepReversal.java
@@ -0,0 +1,38 @@
+package com.programmingsquirrel.threestepreversal;
+
+public class ThreeStepReversal {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 9;
+
+        rotateLeft(nums, k);
+    }
+
+    public static void rotateLeft(int[] nums, int k) {
+        int n = nums.length;
+
+        k = k % n; // This operation is to avoid doing extra movements.
+
+        // Invert the array
+        reverse(nums, 0, n - 1);
+        // Invert the first elements
+        reverse(nums, 0, k - 1);
+        // Invertir the last elements
+        reverse(nums, k, n - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+
+        /* The idea is to reverse the values that indicates from the start and end,
+         * in other words switch the first element (start) with the final element (end)
+         */
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
